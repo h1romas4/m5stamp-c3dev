c27419e1b18fc62d0eacfef81687a70972de6d13
h1romas4 <h1romas4@gmail.com>
2022-03-16 11:48:57 +0900

    lipstick

--- wasm/web/main.js
+++ wasm/web/main.js
@@ -1,5 +1,3 @@
-import { VgmPlay, XgmPlay, setWasmExport } from "../dist/app.wasm";
-
 /**
  * Canvas settings
  */
@@ -11,14 +9,23 @@ const CANVAS_FONT_SIZE = 14;
  * Canvas
  * @type {HTMLCanvasElement}
  */
- let canvas;
+let canvas;
 
- /**
-  * CanvasContext
-  * @type {CanvasRenderingContext2D}
-  */
- let canvasContext;
+/**
+ * CanvasContext
+ * @type {CanvasRenderingContext2D}
+ */
+let canvasContext;
 
+/**
+ * WebAssembly Exports
+ * @type {WebAssembly.Instance.exports}
+ */
+let wasmExports;
+
+/**
+ * Setting Canvas
+ */
 (function() {
     canvas = document.getElementById('screen');
     canvas.setAttribute('width', CANVAS_WIDTH);
@@ -27,15 +34,27 @@ const CANVAS_FONT_SIZE = 14;
     canvas.style.heigth = CANVAS_HEIGHT * 2 + "px";
     canvasContext = canvas.getContext('2d');
     canvasContext.font = `${CANVAS_FONT_SIZE}px sans-serif`;
-    // canvasContext.fillStyle = '#00a040';
-    // canvasContext.fillRect(0, 0, CANVAS_WIDTH - 1, CANVAS_HEIGHT - 1);
 })();
 
-(async function() {
+/**
+ * Load WebAssembly
+ */
+async function loadWasm() {
     const response = await fetch(new URL('../dist/app.wasm', import.meta.url));
     const responseArrayBuffer = new Uint8Array(await response.arrayBuffer());
     const wasm_bytes = new Uint8Array(responseArrayBuffer).buffer;
     let module = await WebAssembly.compile(wasm_bytes);
+    const instance = await WebAssembly.instantiate(module, {
+        ...createImports()
+    });
+    wasmExports = instance.exports;
+};
+
+/**
+ * Create Imports C3DEV interface
+ * @returns imports
+ */
+function createImports() {
     let imports = [];
     imports['env'] = {
         'abort': (i0, i1, i2, i3) => {
@@ -55,12 +74,30 @@ const CANVAS_FONT_SIZE = 14;
             console.log(`c3dev.draw_string: ${i0}, ${i1}, ${i2}, ${i3}`);
         },
         'now': () => {
-            return Math.random();
+            return Date.now();
         }
     };
-    const instance = await WebAssembly.instantiate(module, {
-        ...imports
-    });
-    console.log(instance.exports);
-    instance.exports.clock(80, 64, 64);
+
+    return imports;
+}
+
+/**
+ * Convert RGB565 to 888
+ * @param {string} rgb565
+ */
+function convertRGB565to888String(rgb565) {
+    const r5 = rgb565 & 0b1111100000000000 >> 11;
+    const g6 = rgb565 & 0b0000011111100000 >> 5;
+    const b6 = rgb565 & 0b0000000000011111;
+    const r8 = (r5 * 255 + 15) / 31;
+    const g8 = (g6 * 255 + 31) / 63;
+    const b8 = (b5 * 255 + 15) / 31;
+}
+
+/**
+ * Main
+ */
+(async function() {
+    await loadWasm();
+    wasmExports.clock(80, 64, 64);
 })();
