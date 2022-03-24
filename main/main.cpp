2070370bba8a5aac6d65e13eedf65c03da5bca64
h1romas4 <h1romas4@gmail.com>
2022-03-24 21:34:10 +0900

    sync ntp and reboot

--- main/main.cpp
+++ main/main.cpp
@@ -18,12 +18,6 @@ static const char *TAG = "main.cpp";
  */
 SPIClass *spi = &SPI;
 
-
-/**
- * NVS member
- */
-Preferences preferences;
-
 /**
  * LCD member
  */
@@ -38,6 +32,39 @@ font_render_t font_render;
  * Wasm3 member
  */
 boolean enable_wasm = false;
+
+void sync_wifi_ntp(void)
+{
+    Preferences preferences;
+
+    if(!preferences.begin("wifi", true)) return;
+
+    String ssid = preferences.getString("ssid");
+    String passwd = preferences.getString("passwd");
+
+    ESP_LOGI(TAG, "Connect to %s", ssid);
+    WiFi.begin(ssid.c_str(), passwd.c_str());
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(200);
+    }
+    ESP_LOGI(TAG, "Connected!");
+    configTime(9 * 3600L, 0, "ntp1.jst.mfeed.ad.jp", "ntp2.jst.mfeed.ad.jp", "ntp3.jst.mfeed.ad.jp");
+    // Wait Time Sync
+    struct tm timeInfo;
+    while(true) {
+        getLocalTime(&timeInfo);
+        if(timeInfo.tm_year > 0) {
+            break;
+        }
+        delay(500);
+        ESP_LOGI(TAG, "waiting time sync..(%d)", timeInfo.tm_year);
+    }
+    ESP_LOGI(TAG, "Configured time from NTP");
+    WiFi.disconnect();
+    // not enough memory..
+    ESP_LOGI(TAG, "Restart ESP32C3");
+    esp_restart();
+}
 
 void setup(void)
 {
@@ -74,18 +101,8 @@ void setup(void)
     // draw_sdcard_png("/M5STACK/TEST10-3.PNG", 80, 60);
 
     // Test NVS and Wifi (Push SW1)
-    if(digitalRead(C3DEV_SW1) == 0 && preferences.begin("wifi", true)) {
-        String ssid = preferences.getString("ssid");
-        String passwd = preferences.getString("passwd");
-
-        ESP_LOGI(TAG, "Connect to %s", ssid);
-        WiFi.begin(ssid.c_str(), passwd.c_str());
-        while (WiFi.status() != WL_CONNECTED) {
-            delay(200);
-        }
-        ESP_LOGI(TAG, "Connected!");
-        configTime(9 * 3600L, 0, "ntp1.jst.mfeed.ad.jp", "ntp2.jst.mfeed.ad.jp", "ntp3.jst.mfeed.ad.jp");
-        ESP_LOGI(TAG, "Configured time from NTP");
+    if(digitalRead(C3DEV_SW1) == 0) {
+        sync_wifi_ntp();
     }
 
     // Test WebAssembly
