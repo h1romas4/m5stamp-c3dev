2ac9a97496b360c17675b1a1f309a8eeaff9eb2f
h1romas4 <h1romas4@gmail.com>
2022-03-29 22:07:12 +0900

    add i2c comment

--- main/test_i2c_gpio1819.cpp
+++ main/test_i2c_gpio1819.cpp
@@ -16,11 +16,14 @@ QMP6988 qmp6988;
 
 void init_i2c_gpio1819(void)
 {
-    // https://community.m5stack.com/topic/3715/m5stamp-c3-i2c-interface
-    // But I'm still unable to read from the I2C device. I've got other stuff working on the board
-    // (like a fan-control, and a water pump) but I2C and Servos are steadfastly refusing to function.
-
-    // Disable JTAG (It seems to be necessary for other GPIOs as well)
+    // components/unitenv/UNIT_ENV/src/SHT3X.cpp
+    // SHT3X::SHT3X(uint8_t address)
+    // {
+    //     Wire.begin();
+    //     _address=address;
+    // }
+    //
+    // Force i2c reassignment because Wire is initialized in the SHT3X constructor.
     i2c_set_pin(0, I2C_SDA, I2C_SCL, true, true, I2C_MODE_MASTER);
 
     Wire.begin(I2C_SDA, I2C_SCL);
