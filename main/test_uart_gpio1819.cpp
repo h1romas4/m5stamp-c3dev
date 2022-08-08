8b4b558fbb507c7f90eb639b96af40eb566aa6ba
h1romas4 <h1romas4@gmail.com>
2022-08-08 21:42:35 +0900

    add test GPS

--- main/test_uart_gpio1819.cpp
+++ main/test_uart_gpio1819.cpp
@@ -11,30 +11,41 @@ static const char *TAG = "test_uart_gpio1819.cpp";
 //  https://docs.m5stack.com/en/unit/gps
 HardwareSerial GPSRaw(1);
 
+/* GPS handle */
+lwgps_t hgps;
+
 void init_uart_gpio1819(void)
 {
     GPSRaw.begin(9600);
     // Set pin (connected reverse)
     GPSRaw.setPins(/* GPS TXD (yellow) */ 19, /* GPS RXD (write) */ 18);
+    // Init GPS
+    lwgps_init(&hgps);
 
-    ESP_LOGI(TAG, "Hardware serial initialized.");
+    ESP_LOGI(TAG, "Hardware serial and lwgps initialized.");
 }
 
 void get_i2c_unitgps_data()
 {
+    uint8_t buffer[255];
+
     while(GPSRaw.available() > 0) {
-        // // encode GPS
-        // if(gps.encode(GPSRaw.read())) {
-        //     // output GPS data
-        //     if(gps.location.isValid()) {
-        //         ESP_LOGI(TAG, "Location(lat, lng): (%lf, %lf)", gps.location.lat(), gps.location.lng());
-        //     }
-        //     if(gps.date.isValid()) {
-        //         ESP_LOGI(TAG, "Date: %d/%d/%d", gps.date.year(), gps.date.month(), gps.date.day());
-        //     }
-        //     if(gps.time.isValid()) {
-        //         ESP_LOGI(TAG, "Time: %d:%d:%d.%d", gps.time.hour(), gps.time.minute(), gps.time.second(), gps.time.centisecond());
-        //     }
-        // }
+        size_t size = GPSRaw.read(buffer, sizeof(buffer));
+        lwgps_process(&hgps, buffer, size);
+        if(hgps.is_valid == 1) {
+            ESP_LOGI(TAG, "Latitude: %f degrees", hgps.latitude);
+            ESP_LOGI(TAG, "Longitude: %f degrees", hgps.longitude);
+            ESP_LOGI(TAG, "Altitude: %f meters", hgps.altitude);
+
+            for(uint8_t i = 0; i < 12; i++) {
+                ESP_LOGI(TAG, "GSA satellites_ids[%d]=%d", i, hgps.satellites_ids[i]);
+            }
+            for(uint8_t i = 0; i < 12; i++) {
+                uint8_t num = hgps.sats_in_view_desc[i].num;
+                ESP_LOGI(TAG, "GSV %d, elevation %d", num, hgps.sats_in_view_desc[i].elevation);
+                ESP_LOGI(TAG, "GSV %d, azimuth %d", num, hgps.sats_in_view_desc[i].azimuth);
+                ESP_LOGI(TAG, "GSV %d, snr %d", num, hgps.sats_in_view_desc[i].snr);
+            }
+        }
     }
 }
